317fd48a761b7adb6e5d6dc3bae37bf7c924a1ee
AL_SCHOOL <61769442+AL-SCHOOL@users.noreply.github.com>
2021-11-14 16:44:46 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ function submit() {
   var book = document.getElementById("book").value;
   var author = document.getElementById("author").value;
   
-  document.getElementById("title").innerHTML = title;
+  document.getElementById("title").innerHTML = book;
   document.getElementById("text").innerHTML = "Inside of the book " + book + " by " + author + "...";
 }
